0bf5f74e0d0f02355b9e7742bda135f2270e3f95
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2021-10-12 01:27:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function openFunction(){
+  document.getElementById("menu").style.width="300px";
+  document.getElementById("mainbox").style.marginLeft="300px";
+  document.getElementById("mainbox").innerHTML="";
+ }
+function closeFunction(){
+ document.getElementById("menu").style.width="0px";
+ document.getElementById("mainbox").style.marginright="0px";
+ document.getElementById("mainbox").innerHTML="";
+}
